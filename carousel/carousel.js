b10cf3a4117282af8f4513c5fe9be3ce5cbbe727
Matthew Kulp <contact@matthewkulp.com>
2015-09-02 21:43:22 -0700

    hello - big changes!
    - file restructuring
    - all kinds of animation frameworks coming and going
    - got the gist of the greeting animation

---
+++ carousel/carousel.js
@@ -0,0 +1,13 @@
+// next function ... will allow for the user to click on the current image and advance to the next.
+//
+// activeImage function ... this will display only the ONE active image and hide all the rest.
+//
+// auto-cycle function ... this will allow me to input a time and have the images change based on that time. they will cycle in order and then repeat
+//
+// auto-cycle STOP function ... this will allow me to stop the autocycle when clicked
+//
+// loop function ... this will allow the auto-cycle to start over when it passes the last item in the carousel. this will allow the next function to start over at the first image too.
+//
+// loading function ... this will show a loading animation until all of the images are loaded
+//
+// increment count function ... this will make it so that the items in the array are automatically numbered and those numbers are actively injected into the DOM
