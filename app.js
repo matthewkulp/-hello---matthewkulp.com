21fa8f0cb9cf8d2aac2abdecd9e2e9b439f2c3d0
Matthew Kulp <contact@matthewkulp.com>
2015-09-18 01:37:44 -0700

    carousel-refactored
    - handling the carousel SO much better.
    - So now there is a carousel template in HTML and I inject the images
    and descriptions into.
    - This means that the page is rendered perfectly without javascript to
    make sure the height is correct and that it’s correct on resize.
    - The amount of code required is significantly reduced and so
    performance is significantly enhanced.

--- app.js
+++ app.js
@@ -1,68 +1,33 @@
-	var tStart = 600 // Start Y in px
-		, tEnd = 1800   // End Y in px
-		, cStart = [180, 170, 97] // Color 1
-		, cEnd = [147, 197, 83]   // Color 2
-		, cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[1] - cStart[0]];
+var tStart = 600 // Start Y in px
+	, tEnd = 1800   // End Y in px
+	, cStart = [180, 170, 97] // Color 1
+	, cEnd = [147, 197, 83]   // Color 2
+	, cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[1] - cStart[0]];
 
-	var aboutBG = $('#aboutBG');
-	var mailchimp = $('#mailchimp input');
-
-
-
-
-	var items = $("#carousel ul").children("li");
-	var pictures = $("#carousel ul li .imageContainer").children();
-	var carousel = $('#carousel');
-	items.eq(0).addClass('active');
-	var activePic = $('.active').index(); // returns a number
-
-
-	function setCarouselHeight() {
-		var activePic = $('.active').index(); // returns a number
-		var itemHeight = items.eq(activePic).css("height");
-
-		carousel.css("height", itemHeight);
-
-		var imageHeight = pictures.css("height");
-		 $('.next').css("height", imageHeight);
-		$('.previous').css("height", imageHeight);
-	};
-
-	setCarouselHeight();
+var aboutBG = $('#aboutBG');
+var mailchimp = $('#mailchimp input');
 
 
 
 
-	// Reset carousel height on resize for non-touch devices.
-	if (Modernizr.touch == false) {
-		$( window ).resize(function() {
-			 setCarouselHeight();
-		});
-	}
+// Check for Chrome on iOS and ignore scroll events.
+if(navigator.userAgent.match('CriOS')){
+
+} else {
+	// Animate background color
+	$(document).scroll(function() {
+		var p = ($(this).scrollTop() - tStart) / (tEnd - tStart); // % of transition
+		p = Math.min(1, Math.max(0, p)); // Clamp to [0, 1]
+		var cBg = [Math.round(cStart[0] + cDiff[0] * p), Math.round(cStart[1] + cDiff[1] * p), Math.round(cStart[2] + cDiff[2] * p)];
+	     aboutBG.css('background-color', 'rgb(' + cBg.join(',') +')');
+	});
+
+}
 
 
 
 
-	$('#greeting').css('height', 'auto');
-
-	// Check for Chrome on iOS and ignore scroll events.
-	if(navigator.userAgent.match('CriOS')){
-
-	} else {
-		// Animate background color
-		$(document).scroll(function() {
-			var p = ($(this).scrollTop() - tStart) / (tEnd - tStart); // % of transition
-			p = Math.min(1, Math.max(0, p)); // Clamp to [0, 1]
-			var cBg = [Math.round(cStart[0] + cDiff[0] * p), Math.round(cStart[1] + cDiff[1] * p), Math.round(cStart[2] + cDiff[2] * p)];
-		     aboutBG.css('background-color', 'rgb(' + cBg.join(',') +')');
-		});
-
-	}
-
-
-
-
-	// Newsletter onClick behavior
-	mailchimp.on('click', function(){
-		mail.attr('placeholder', '');
-	})
+// Newsletter onClick behavior
+mailchimp.on('click', function(){
+	mailchimp.attr('placeholder', '');
+})
--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,264 +1,260 @@
-	var previousHitbox = $('.previous');
-	var nextHitbox = $('.next');
-	var mobileGifs = $('.mobileGif');
 
 
-	// Detect Touch Device.
-	if (Modernizr.touch) {
 
-		// Get rid of .previous hitbox so a touch event advances in one direction.
-		previousHitbox.hide();
-		nextHitbox.css('width', '100%');
 
-		// Remove mp4s
-		$('.video').remove();
+var previousHitbox = $('.previous');
+var nextHitbox = $('.next');
+var mobileGifs = $('.mobileGif');
 
-		// Take div attribute and turn it into an image and insert it in the div.
-		mobileGifs.map(function() {
-			$(this).append('<img src="' + $(this).attr('url') + '" />');
-		});
 
-	} else {
-		mobileGifs.remove();
+
+
+// Detect Touch Device.
+if (Modernizr.touch) {
+
+	// Get rid of .previous hitbox so a touch event advances in one direction.
+	previousHitbox.hide();
+	nextHitbox.css('width', '100%');
+
+
+	// Remove mp4s
+	$('.video').remove();
+
+
+	// Take div attribute and turn it into an image and insert it in the div.
+	mobileGifs.map(function() {
+		$(this).append('<img src="' + $(this).attr('url') + '" />');
+	});
+
+} else {
+
+	mobileGifs.remove();
+
+};
+
+
+
+
+//Initializations
+var carousel = $('#carousel');
+var items = $("#carousel ul").children("li");
+var pictures = $("#carousel ul li").children(".imageContainer");
+var descriptions = $("#carousel ul li .caption").children(".description");
+var inViewImageContainer = $('#carousel .imageContainer');
+var loader = $(".loader");
+var animationComplete = false;
+items.eq(0).addClass('active');
+var currentItem = $('.active').index();
+var currentImage = $('.currentImage');
+var nextImage = $('.nextImage');
+var nextDescription = $('.nextDescription');
+var count = $('.count');
+var autoCycle;
+
+
+
+
+enterExitTime = 200;
+descriptionYChange = 9;
+
+function goTo(itemIndex){
+	nextItem = itemIndex;
+	animationComplete = false;
+
+	//Count Change
+	countChange(itemIndex);
+
+	//Image Changes
+	nextImage.empty();
+	nextImage.css('opacity', '0');
+	nextImage.html(pictures.eq(nextItem).html());
+	if (nextImage.children().attr('isVideo') == 'true') {
+		var video = nextImage.children();
+		video[0].play();
 	};
-
-
-
-
-	//Initializations
-	var carousel = $('#carousel');
-	var items = $("#carousel ul").children("li");
-	var pictures = $("#carousel ul li").children(".imageContainer");
-	var pictureInnards = $('#carousel ul li .imageContainer').children();
-	var descriptions = $("#carousel ul li .caption").children(".description");
-	var counts = $("#carousel ul li .caption").children(".count");
-	var loader = $(".loader");
-	var animationComplete = false;
-	items.eq(0).addClass('active');
-	var currentPic = $('.active').index();
-	var exitingPic;
-	var previousPic;
-	var nextHitboxPic;
-	var autoCycle;
-
-
-
-
-	// Animations
-	enterExitTime = 200;
-
-	function imageEnters (enteringPic) {
-
-		pictures.css("z-index", "0");
-		pictures.eq(currentPic).css("z-index", "1");
-		pictures.eq(enteringPic).css("z-index", "2");
-
-		if (pictureInnards.eq(enteringPic).attr('isVideo') == 'true') {
-			pictureInnards[enteringPic].play();
-		};
-
-		pictures.eq(enteringPic).velocity("fadeIn", {
-			duration: enterExitTime * 2,
-			complete: function(){
-				pictures.eq(previousPic).css("display", "none");
-				if (pictureInnards.eq(previousPic).attr('isVideo') == 'true') {
-					pictureInnards[previousPic].load();
-				};
-			}
-		});
-
-	};
-
-
-
-
-	function countChange (picIndex) {
-		counts.eq(picIndex).html(function() {
-			return (picIndex + 1) + "/" + pictures.length;
-		});
-	}
-
-
-
-
-	function carouselIntroduction(picIndex) {
-		items.css('opacity', '1');
-		nextHitbox.css('display', 'block');
-		previousHitbox.css('display', 'block');
-
-		descriptions.css('opacity', '0');
-		descriptionEnters(picIndex);
-
-		countChange(picIndex);
-		counts.velocity("fadeIn", { duration: enterExitTime });
-
-		imageEnters(picIndex);
-		animationComplete = true;
-	};
-
-
-
-
-	function next(){
-
-		if (currentPic < pictures.length-1 && animationComplete){
-			goTo(currentPic + 1);
-
-		} else if (animationComplete) {
-			goTo(0)
-		}
-	};
-
-
-
-
-	function previous(){
-
-		if (currentPic == 0 && animationComplete){
-			goTo(pictures.length-1);
-		} else if (animationComplete){
-			goTo(currentPic - 1);
-		}
-	}
-
-
-
-
-	// Description Animation
-	descriptionYChange = 9;
-
-	function descriptionExits (picIndex) {
-		descriptions.eq(picIndex).velocity(
-			{
-			opacity: [0, "ease-in", 1],
-			translateY: [descriptionYChange, 0],
+	nextImage.velocity('fadeIn', {
+		duration: enterExitTime * 2,
+		complete: function() {
+				currentImage.html(nextImage.html());
 			},
-			{
-			duration: enterExitTime,
-			}
-		);
-	}
+		}
+	)
 
-	function descriptionEnters(picIndex) {
-		descriptions.eq(picIndex).velocity(
-			{
-			opacity: [1, "ease-in", 0],
-			translateY: [0, descriptionYChange],
-			},
-			{
-			duration: enterExitTime,
-			delay: enterExitTime,
-			complete:
-				function() {
+	// Description Changes
+	nextDescription.velocity(
+		{
+		opacity: [0, "ease-in", 1],
+		translateY: [descriptionYChange, 0],
+		},
+		{
+		duration: enterExitTime,
+		complete: function() {
+			nextDescription.html(descriptions.eq(nextItem).html());
+			nextDescription.velocity(
+				{
+				opacity: [1, "ease-in", 0],
+				translateY: [0, descriptionYChange],
+				},
+				{
+				duration: enterExitTime,
+				complete: function() {
 					animationComplete = true;
-				},
-			}
-		);
-	};
-
-
-
-
-	function goTo(picIndex){
-		nextPic = picIndex;
-		previousPic = currentPic;
-		animationComplete = false;
-
-		//Count Change
-		counts.css("opacity", "0");
-		countChange(nextPic);
-		counts.eq(nextPic).css("opacity", "1");
-
-		//Picture Changes
-		imageEnters(nextPic);
-
-		//Description Change
-		descriptionExits(currentPic);
-		descriptionEnters(nextPic);
-
-		//Make the picture in view .active and 'currentPic'
-		items.removeClass('active')
-		.eq(nextPic).addClass('active');
-		currentPic = nextPic;
-	};
-
-
-
-
-	// Click Behavior
-	nextHitbox.on('click', function() {
-		if (animationComplete) {
-			clearInterval(autoCycle);
-			next();
-			return false;
+				}
+			})
 		}
 	});
 
-	previousHitbox.on('click', function() {
-		if (animationComplete) {
+
+	//Make the picture in view .active and 'currentItem'
+	items.removeClass('active')
+		.eq(nextItem).addClass('active');
+	currentItem = nextItem;
+
+
+};
+
+
+
+
+function countChange (itemIndex) {
+	count.html(function() {
+		return (itemIndex + 1) + "/" + pictures.length;
+	});
+}
+
+
+
+
+function carouselIntroduction(itemIndex) {
+	// nextImage.html(pictures.eq(currentItem).html());
+	nextHitbox.css('display', 'inherit');
+	previousHitbox.css('display', 'inherit');
+
+	goTo(itemIndex);
+};
+
+
+
+
+// Loading Behavior
+loaderEnterExitTime = 500;
+
+loader.velocity("fadeIn", {
+	duration: loaderEnterExitTime
+});
+
+setTimeout(function () {
+	pictures.eq(currentItem).imagesLoaded( function() {
+	      loader.velocity("fadeOut",
+	      {
+	      duration: loaderEnterExitTime,
+	      complete: function() {
+				carouselIntroduction(currentItem);
+				// // Set AutoCycle
+				autoCycle = setInterval( next, 6000);
+			},
+		});
+	})
+}, 1100);
+
+
+
+
+function next(){
+
+	if (currentItem < pictures.length-1 && animationComplete){
+		goTo(currentItem + 1);
+
+	} else if (animationComplete) {
+		goTo(0)
+	}
+
+};
+
+
+
+
+function previous(){
+
+	if (currentItem == 0 && animationComplete){
+		goTo(pictures.length-1);
+	} else if (animationComplete){
+		goTo(currentItem - 1);
+	}
+
+}
+
+
+
+
+// Click Behavior
+nextHitbox.on('click', function() {
+
+	if (animationComplete) {
+		clearInterval(autoCycle);
+		next();
+		return false;
+	}
+
+});
+
+
+
+
+previousHitbox.on('click', function() {
+
+	if (animationComplete) {
+		clearInterval(autoCycle);
+		previous();
+		return false;
+	}
+
+});
+
+
+
+
+//Swipe Behavior
+inViewImageContainer.on('swipeleft', function () {
+
+	clearInterval(autoCycle);
+	if (animationComplete) {
+		next();
+	}
+
+});
+
+
+
+
+inViewImageContainer.on('swiperight', function () {
+
+	clearInterval(autoCycle);
+	if (animationComplete) {
+		previous();
+	}
+
+});
+
+
+
+
+$(document).keydown(function(e) {
+
+	switch(e.which) {
+		case 37: // left arrow key
 			clearInterval(autoCycle);
 			previous();
-			return false;
-		}
-	});
+			break;
 
-
-
-
-	//Swipe Behavior
-	carousel.on('swipeleft', function () {
-		clearInterval(autoCycle);
-		if (animationComplete) {
+		case 39: // right arrow key
+			clearInterval(autoCycle);
 			next();
-		}
-	});
+			break;
 
-	carousel.on('swiperight', function () {
-		clearInterval(autoCycle);
-		if (animationComplete) {
-			previous();
-		}
-	});
+		default: return; // exit this handler for other keys
+	}
 
+	e.preventDefault(); // prevent the default action (scroll / move caret)
 
-
-
-	$(document).keydown(function(e) {
-		switch(e.which) {
-			case 37: // left arrow key
-				clearInterval(autoCycle);
-				previous();
-				break;
-
-			case 39: // right arrow key
-				clearInterval(autoCycle);
-				next();
-				break;
-
-			default: return; // exit this handler for other keys
-		}
-		e.preventDefault(); // prevent the default action (scroll / move caret)
-	});
-
-
-
-
-	// Loading Behavior
-	loaderEnterExitTime = 500;
-
-
-	loader.velocity("fadeIn", {duration: loaderEnterExitTime});
-
-	setTimeout(function () {
-		pictures.eq(currentPic).imagesLoaded( function() {
-		      loader.velocity("fadeOut",
-		      {
-		      duration: loaderEnterExitTime,
-		      complete: function() {
-					carouselIntroduction(currentPic);
-					// Set AutoCycle
-					autoCycle = setInterval( next, 6000);
-				},
-			});
-		})
-	}, 1200);
+});
